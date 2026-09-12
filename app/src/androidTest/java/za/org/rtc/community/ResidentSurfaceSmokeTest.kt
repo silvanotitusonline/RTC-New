f89d4c07f5bb3e322ecea4c22f81923f9701006e
silvanotitusonline <silvanotitusonline@gmail.com>
2026-09-13 01:51:16 +0200

    fix(androidTest): use member nonexistence assertion

--- app/src/androidTest/java/za/org/rtc/community/ResidentSurfaceSmokeTest.kt
+++ app/src/androidTest/java/za/org/rtc/community/ResidentSurfaceSmokeTest.kt
@@ -1,6 +1,5 @@
 package za.org.rtc.community
 
-import androidx.compose.ui.test.assertDoesNotExist
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
