cbe3c75e3ff46e9a6e0a6310bf6fffa58df26049
silvanotitusonline <silvanotitusonline@gmail.com>
2026-09-13 20:10:40 +0200

    refactor(marketplace): remove service centre switch

--- app/src/main/java/za/org/rtc/community/feature/marketplace/presentation/MarketHubScreen.kt
+++ app/src/main/java/za/org/rtc/community/feature/marketplace/presentation/MarketHubScreen.kt
@@ -1,14 +1,10 @@
 package za.org.rtc.community.feature.marketplace.presentation
 
 import androidx.compose.runtime.Composable
-import za.org.rtc.community.navigation.RtcRoute
 
 @Composable
 fun MarketHubScreen(
     onNavigate: (String) -> Unit,
 ) {
-    MarketplaceHomeRoute(
-        onNavigate = onNavigate,
-        onSwitchToServices = { onNavigate(RtcRoute.SERVICE_CENTRE_HOME) },
-    )
+    MarketplaceHomeRoute(onNavigate = onNavigate)
 }
